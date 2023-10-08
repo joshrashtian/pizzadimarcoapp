a37501cefbd139c610587dcc6ce82f33980393e5
Joshua Rashtian <joshrashtian1@gmail.com>
2023-10-08 00:50:22 -0700

    Creating our Stack Navigator System (2)

--- components/index.js
+++ components/index.js
@@ -1,21 +1,36 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from '../screens/Home';
-import Welcome from '../screens/Welcome';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../screens/Home";
+import Welcome from "../screens/Welcome";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-function Layout(props) {
-    
-    return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}}>
-                <Stack.Screen name="Home" component={Home}/>
-                <Stack.Screen name="Welcome" component={Welcome}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+function BottomRow() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{ headerShown: false }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{ title: "Home Page" }}
+      />
+    </Tab.Navigator>
+  );
 }
 
+function Layout(props) {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default Layout;
