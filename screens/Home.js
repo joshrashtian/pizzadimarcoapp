87f526c78ee0e3d87f99f9c93312169023b89ebe
Joshua Rashtian <joshrashtian1@gmail.com>
2023-10-10 23:39:41 -0700

    Fancying Up Login System, it is mostly finished

--- screens/Home.js
+++ screens/Home.js
@@ -1,33 +1,32 @@
-import { useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
-import { Stack, useRouter } from 'expo-router';
+import { useState } from "react";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { User } from "firebase/auth";
 
 const Home = () => {
-    return (
-        <SafeAreaView style={styles.container}>
-        <View style={styles.container2}>
-            <Text>Welcome to Pizza DiMarco!</Text>
-        </View>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Welcome back, user!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignContent: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#EEE',
-    },
-    container2: {
-        padding: 30,
-        backgroundColor: '#CCC',
-        marginHorizontal: 10,
-        borderRadius: 30,
-    },
-    hometext: {
-        
-    }
-})
+  container: {
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: "#EEE",
+  },
+  header: {
+    alignContent: "center",
+  },
+  title: {
+    fontFamily: "LouisGeorgeCafe",
+    fontSize: 30,
+  },
+});
 
 export default Home;
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -14,55 +14,83 @@ import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
-  onAuthStateChanged
+  onAuthStateChanged,
 } from "firebase/auth";
-
 
 const Welcome = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
+  //Setting up the sign in function
   const signIn = async () => {
     setLoading(true);
     try {
       const response = await signInWithEmailAndPassword(auth, email, password);
       console.log(response);
-    } catch(error) {
+    } catch (error) {
       console.log(error);
-      alert('Sign in failed: ' + error.message);
+      alert("Sign in failed: " + error.message);
     } finally {
       setLoading(false);
-    } 
-  }
+    }
+  };
 
+  //Setting up the register function
   const signUp = async () => {
     setLoading(true);
     try {
-      const response = await createUserWithEmailAndPassword(auth, email, password);
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       console.log(response);
-    } catch (error){
-      alert('Sign in failed: ' + error.message);
+    } catch (error) {
+      alert("Sign in failed: " + error.message);
     } finally {
       setLoading(false);
-    } 
-  }
+    }
+  };
 
+
+  //Handles if the user has been logged in.
   onAuthStateChanged(auth, (user) => {
     if (user) {
-      navigation.navigate('Home')
+      navigation.navigate("Home");
       const uid = user.uid;
-      // ...
     } else {
-      // User is signed out
-      // ...
+      navigation.navigate("Welcome");
     }
   });
 
+
+  //Login Screen, what the user sees.
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.container2}>
+        <Text
+          style={{
+            fontSize: 40,
+            fontFamily: "lemonmilk",
+            textAlign: "center",
+            color: "#F44",
+            textShadowRadius: 0.3,
+            textShadowColor: "#000",
+          }}
+        >
+          Pizza diMarco
+        </Text>
         <Text style={styles.hometext}>Account Login</Text>
+        <View
+          style={{
+            borderWidth: 0.7,
+            borderColor: "#000",
+            borderRadius: 10,
+            marginVertical: 5,
+            marginHorizontal: 5,
+          }}
+        ></View>
         <View style={styles.logincontainer}>
           <TextInput
             placeholder="email"
@@ -94,8 +122,11 @@ const Welcome = ({ navigation }) => {
   );
 };
 
+//Exporting our function
 export default Welcome;
 
+
+//Style Sheet
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -103,9 +134,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#EEE",
   },
   container2: {
-    padding: 80,
+    padding: 40,
     backgroundColor: "#CCC",
-    marginHorizontal: 10,
+    marginHorizontal: 5,
     borderRadius: 30,
   },
   hometext: {
@@ -115,16 +146,17 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   input: {
-    padding: 13,
+    padding: 15,
     borderRadius: 10,
-    borderColor: "#EEE",
-    borderWidth: 2,
     margin: 3,
     fontFamily: "LouisGeorgeCafe",
     backgroundColor: "#FFF",
+    shadowRadius: 0.2,
+    shadowColor: "#000",
+    shadowOpacity: "80%",
+    shadowOffset: 1,
   },
   buttonContainer: {
-    flexDirection: "row",
     marginTop: 10,
     justifyContent: "center",
   },
@@ -132,16 +164,23 @@ const styles = StyleSheet.create({
     padding: 10,
     paddingHorizontal: 40,
     borderRadius: 10,
-    marginHorizontal: 10,
+    margin: 5,
     backgroundColor: "#DDD",
+    borderShadowRadius: 1,
+    shadowRadius: 0.8,
+    shadowColor: "#000",
+    shadowOpacity: "30%",
+    shadowOffset: 1,
   },
   buttontext: {
     fontFamily: "lemonmilk",
     color: "#050",
+    textAlign: "center",
   },
   buttonOutline: {},
   buttonOutlineText: {
     fontFamily: "lemonmilk",
     color: "#500",
+    textAlign: "center",
   },
 });
